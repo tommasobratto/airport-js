4d6abba5ef1b1f61f31a943d71016b0a9ec94e67
tommasobratto <tbratto22@gmail.com>
2015-03-03 21:37:21 +0000

    added test with stubs for random weather, test passes

--- spec/WeatherSpec.js
+++ spec/WeatherSpec.js
@@ -3,7 +3,8 @@ describe('weather', function() {
   var weather = new Weather();
 
   it('can be sunny', function() {
-    expect(weather.randomConditions()).toEqual("sunny")
+    spyOn(weather, 'randomNumberGenerator').and.returnValue(2)
+    expect(weather.randomConditions(this.randomNumberGenerator)).toEqual("sunny")
   });
 
 });
--- src/Weather.js
+++ src/Weather.js
@@ -5,9 +5,9 @@ Weather.prototype.randomNumberGenerator = function(){
   return Math.floor((Math.random() * 10) + 1);
 };
 
-Weather.prototype.randomConditions = function() {
-  if(this.randomNumberGenerator > 1 && this.randomNumberGenerator < 6) { 
-    return "sunny";
+Weather.prototype.randomConditions = function(randomNumberGenerator) {
+  if(this.randomNumberGenerator() > 1 && this.randomNumberGenerator() < 6) {
+    return "sunny"; 
   } else { 
     return "stormy";   
   }
