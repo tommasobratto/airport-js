45e0187993ed5594031a0c451240b047818d2d5b
tommasobratto <tbratto22@gmail.com>
2015-03-03 16:58:28 +0000

    airport dock function test passing

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -1,10 +1,21 @@
 describe('airport', function() {
 
-  var airport = new Airport;
+  var airport = new Airport();
+  var plane;
+
+  beforeEach( function() {
+    plane = jasmine.createSpyObj('plane', [ 'land', 'takeOff'])
+  });
 
   it('should dock a plane', function() {
     airport.dock(plane);
-    expect(aiport.planes).toEqual([plane])
+    expect(airport.planes).toEqual([plane])
   });
+
+  it('should allow a plane to take off', function(){
+    airport.dock(plane)
+    airport.release(plane)
+    expect(airport.planes).toEqual([])
+  })
 
 });
