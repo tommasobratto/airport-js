ceeefca5358ede244527f1e23c45372c712ac443
Vanessa <virgitti@gmail.com>
2015-03-03 16:29:48 +0000

    test passing for flying status

--- spec/PlaneSpec.js
+++ spec/PlaneSpec.js
@@ -1,1 +1,15 @@
 
+describe('plane', function() {
+
+  var plane = new Plane;
+
+  it('have a flying status when created', function() {
+    expect(plane.isFlying).toBe(true)
+  });
+
+  it("doesn't have a flying status when it lands", function() {
+    plane.land();
+    expect(plane.isFlying).toBe(false)
+  });
+
+});
