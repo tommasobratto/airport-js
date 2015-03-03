05a82e659dfd856696b8f71f3d7ceeffb7fd1106
tommasobratto <tbratto22@gmail.com>
2015-03-03 22:50:58 +0000

    added Empty and Full methods, tests added for weather conditions

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -7,27 +7,62 @@ describe('airport', function() {
     plane = jasmine.createSpyObj('plane', [ 'land', 'takeOff'])
   });
 
-    describe('the weather is sunny' function() {
+  describe('the weather is sunny', function() {
 
-      it('should dock a plane', function() {
-        airport.dock(plane);
-        expect(airport.planes).toEqual([plane])
-      });
+    it('should dock a plane', function() {
+      
+      airport.dock(plane);
+      expect(airport.planes()).toEqual([plane])
+    
+    });
 
-      it('should allow a plane to take off', function() {
-        airport.dock(plane)
-        airport.release(plane)
-        expect(airport.planes).toEqual([])
-      })
+    it('should allow a plane to take off', function() {
+     
+      airport.dock(plane)
+      airport.release(plane)
+      expect(airport.planes()).toEqual([])
+    
+    })
+
+  });
+
+  describe('the weather is stormy', function() {
+
+    it('cannot allow a plane to land', function() {
+
+      airport.dock(plane)
+      expect(airport.planes()).toEqual([])
 
     });
 
-     describe('the weather is stormy' function() {
+    it('cannot allow a plane to take off', function() {
 
-      it('cannot allow a plane to land' function() {
-
-      });
+      airport.dock(plane)
+      airport.release(plane)
+      expect(airport.planes()).toEqual[plane]
 
     });
+
+  });
+
+  describe('hangar management: ', function() {
+
+    it("should know when it's full", function() { 
+      
+      airport.dock(plane);
+      airport.dock(plane);
+      airport.dock(plane);
+      expect(airport.isFull()).toEqual(true)
+
+    });
+
+    it("should know when it's empty", function() {
+
+      airport.release(plane)
+      expect(airport.isEmpty()).toEqual(true)
+
+    });
+
+  }); 
 
 });
--- src/Airport.js
+++ src/Airport.js
@@ -9,3 +9,12 @@ Airport.prototype.dock = function(plane) {
 Airport.prototype.release = function(plane) {
   this.planes.splice(plane);
 };
+
+Airport.prototype.isFull = function() { 
+  var hangarCapacity = 3;
+  if(this.planes.length === hangarCapacity) return true;
+};
+
+Airport.prototype.isEmpty = function() {
+  if(this.planes.length < 1) return true;
+};
