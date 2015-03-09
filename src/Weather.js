a1a0101f3df2ba7b52b0390abc048dfd5976739d
tommasobratto <tbratto22@gmail.com>
2015-03-09 01:24:25 +0000

    removed comment lines in Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -2,10 +2,6 @@ var Weather = function() {
   this.conditions;
 };
 
-// Weather.prototype.randomNumberGenerator = function() {
-//   return Math.floor((Math.random() * 10) + 1);
-// };
-
 Weather.prototype.randomConditions = function() {
   var randomNumber = Math.floor((Math.random() * 10) + 1);
   if( randomNumber > 1 && randomNumber < 6) {
